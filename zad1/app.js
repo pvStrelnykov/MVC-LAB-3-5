70371e81c0653acb3d6528bcf5a3940e02167c2e
StrelnikovPavlo <strelnikovpavel82@gmail.com>
2024-04-16 17:42:04 +0200

    zadanie 1

---
+++ zad1/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const PORT = 3000
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+	res.send('In progress...')
+})
+
+app.use((req, res) => {
+	res.status(404).send('404 Not Found');
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
+})
---
+++ zad1/models/author.js
@@ -0,0 +1,6 @@
+export class Author {
+	constructor(id, name) {
+			this.id = id
+			this.name = name
+	}
+}
